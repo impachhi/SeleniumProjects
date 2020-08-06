c6aa7a95505bb73f005cca36f0f1a85e09d9d13f
Prasanna <pum547@gmail.com>
2020-08-06 11:42:45 +0530

    first commit

---
+++ Seleniumproject/src/SeleniumDay1/GSearch.java
@@ -0,0 +1,22 @@
+package SeleniumDay1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GSearch {
+
+	public static void main(String[] args) {
+		
+		 System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
+
+         WebDriver driver = new ChromeDriver(); 
+         driver.get("http://www.google.com"); 
+         WebElement element = driver.findElement(By.name("q"));
+         element.sendKeys("Cheese!");
+         element.submit();
+         driver.quit();
+	}
+
+}
